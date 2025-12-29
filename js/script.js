2b06cc34960e0776486dc6e36ce107470ac15cf8
Back jin woo <dkankfk@gmail.com>
2025-12-29 10:47:40 +0900

    더페이스샵 인터렉션 수정_2

--- js/script.js
+++ js/script.js
@@ -160,51 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-/* footer */
-document.querySelectorAll('footer .top-box .left-box ul').forEach(ul => {
-    const tit = ul.querySelector('li.tit');
-    if (!tit) return;
 
-    tit.addEventListener('click', () => {
-    // 다른 섹션 닫기
-    document.querySelectorAll('footer .top-box .left-box ul.open')
-        .forEach(other => { if (other !== ul) other.classList.remove('open'); });
-
-    // 현재 토글
-    ul.classList.toggle('open');
-    });
-});
-
-
-    /* header */
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.querySelector("header .menu-icon .btn-menu a");
-  const closeBtn = document.querySelector("header ul.nav .btn-close");
-  const overlay = document.querySelector(".menu-overlay");
-
-  const openMenu = () => {
-    document.body.classList.add("menu-open");
-    document.body.style.overflow = "hidden"; // ✅ 스크롤 잠금
-  };
-
-  const closeMenu = () => {
-    document.body.classList.remove("menu-open");
-    document.body.style.overflow = ""; // ✅ 원복
-  };
-
-  menuBtn?.addEventListener("click", (e) => {
-    e.preventDefault();
-    openMenu();
-  });
-
-  closeBtn?.addEventListener("click", closeMenu);
-  overlay?.addEventListener("click", closeMenu);
-
-  // ✅ 메뉴 안 링크 누르면 자동 닫기(원하면)
-  document.querySelectorAll("header ul.nav a").forEach(a => {
-    a.addEventListener("click", closeMenu);
-  });
-});
 //ingredients
 gsap.timeline({
     scrollTrigger:{
@@ -339,10 +295,10 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.timeline({
     scrollTrigger: {
       trigger: ".materials",
-      start: "top 70%",
-      end: "top 30%",
+      start: "2% 70%",
+      end: "50% 30%",
       toggleActions: "play none none reverse",
-      // markers: true
+      markers: true
     }
   }).to(items, {
     y: 0,
