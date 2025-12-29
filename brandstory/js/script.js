dde64de549185928617b41a3c7e3a9538a912d1d
Back jin woo <dkankfk@gmail.com>
2025-12-29 10:30:42 +0900

    더페이스샵 링크 수정

--- brandstory/js/script.js
+++ brandstory/js/script.js
@@ -1,16 +1,15 @@
-$(document).on('click', 'a[href="#"]', function(e){
+    $(document).on('click', 'a[href="#"]', function(e){
     e.preventDefault();
 });
 
-new Swiper('.shop-swiper', {
-    loop: false,
-    slidesPerView: "auto",    
-    spaceBetween: 24,
-    slidesOffsetBefore: 0,
-    slidesOffsetAfter: 0,
-    centeredSlides: false,
-    watchOverflow: true,
-    autoHeight : true,
-});
-
-
+        //스크롤에 따라 gnb가 나타나고 사라지는
+var prevScrollTop = 0;
+document.addEventListener('scroll', function(){
+    var nowScrollTop = $(window).scrollTop();   //먼저 현재값을 저장시킨다
+    if(nowScrollTop > prevScrollTop ){
+        $('header').addClass('active');
+    } else{             //아닐경우
+        $('header').removeClass('active');
+    }
+    prevScrollTop = nowScrollTop;  //항상 스크롤값을 전달해야한다
+})
